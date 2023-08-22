36ec7021d037ff13963a9793d6224c93721ba3c2
Umasha Kaumadi <105776548+umasha2000@users.noreply.github.com>
2023-08-22 09:51:50 +0530

    Main Carousel Creation

--- frontend/neja-ecommerce/src/app/app.module.ts
+++ frontend/neja-ecommerce/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { MainCarouselComponent } from './home/main-carousel/main-carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MainCarouselComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/neja-ecommerce/src/app/home/main-carousel/main-carousel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-carousel',
+  templateUrl: './main-carousel.component.html',
+  styleUrls: ['./main-carousel.component.scss']
+})
+export class MainCarouselComponent {
+  carouselData: any
+}
