8bc3fea9a52ead2b2a3f29b6fb11f94f0b901a7a
Umasha Kaumadi <105776548+umasha2000@users.noreply.github.com>
2023-08-23 23:21:48 +0530

    Committing Data Changes

--- frontend/neja-ecommerce/src/app/home/product-slider/product-slider.component.ts
+++ frontend/neja-ecommerce/src/app/home/product-slider/product-slider.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-slider',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-slider.component.scss']
 })
 export class ProductSliderComponent {
-
+  @Input() title: any
+  @Input() products: any
 }
